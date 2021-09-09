60ec523852f2fd4ef2554842c7e4592c6921ec7a
kiran <kiran@DESKTOP-5E8P7V8>
2021-09-09 14:59:31 +0530

    com.configuration

---
+++ Library_System/src/com/configuration/DBConnect.java
@@ -0,0 +1,29 @@
+package com.configuration;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DBConnect {
+
+	static final String driver="com.mysql.cj.jdbc.Driver";
+	static final String url="jdbc:mysql://localhost:3306/library_system";
+	static final String user="root";
+	static final String password="root";
+	
+	public static Connection getConnection()
+	{
+		Connection con =null;
+		try {
+			con=DriverManager.getConnection(url,user,password);
+			}catch(SQLException e)
+		{
+				e.printStackTrace();
+		}
+		
+		return con;
+	}
+	}
+
+
