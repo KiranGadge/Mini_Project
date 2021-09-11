d4cda6646e6bc988432aaa94fd955f74f842f45e
kiran <kiran@DESKTOP-5E8P7V8>
2021-09-11 19:57:04 +0530

    DBConnection file

--- Library_System/src/com/configuration/DBConnect.java
+++ Library_System/src/com/configuration/DBConnect.java
@@ -4,26 +4,21 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
 public class DBConnect {
 
-	static final String driver="com.mysql.cj.jdbc.Driver";
-	static final String url="jdbc:mysql://localhost:3306/library_system";
-	static final String user="root";
-	static final String password="root";
-	
-	public static Connection getConnection()
-	{
-		Connection con =null;
+	static final String driver = "com.mysql.cj.jdbc.Driver";
+	static final String url = "jdbc:mysql://localhost:3306/library_system";
+	static final String user = "root";
+	static final String password = "root";
+
+	public static Connection getConnection() {
+		Connection con = null;
 		try {
-			con=DriverManager.getConnection(url,user,password);
-			}catch(SQLException e)
-		{
-				e.printStackTrace();
+			con = DriverManager.getConnection(url, user, password);
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
-		
+
 		return con;
 	}
-	}
-
-
+}
